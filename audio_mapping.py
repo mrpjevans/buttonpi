ad3328f3f862e749fc18b3103284bdb3dba8ee96
PJ Evans <pj@mrpjevans.com>
2024-10-28 16:36:15 +0000

    Add audio

--- audio_mapping.py
+++ audio_mapping.py
@@ -1,10 +1,10 @@
 audio_mapping = [
     "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav",
-    "01_Flowers_On_Flowers.wav"
+    "02_Legacy.wav",
+    "03_People_and_staff_dynamics.wav",
+    "04_Pressure_and_responsibility.wav",
+    "05_Secrecy.wav",
+    "06_Sourcing_supplies.wav",
+    "07_Technology_challenges.wav",
+    "08_Routine.wav"
 ]
